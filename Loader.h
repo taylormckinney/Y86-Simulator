0e8f1e454f719ea9a77c74e3b356f04ab164a2b2
Cori Ernhart <ernhartcl@appstate.edu>
2019-09-24 17:22:50 -0400

    updated Loader.h

--- Loader.h
+++ Loader.h
@@ -3,8 +3,7 @@ class Loader
 {
    private:
       bool loaded;        //set to true if a file is successfully loaded into memory
-      std::ifstream inf;  //input file handle
-      uint64_t val; 
+      std::ifstream inf;  //input file handle 
    public:
       Loader(int argc, char * argv[]);
       bool isLoaded();
