94b034f73ce716c8a2f357cd7beea5acdfc3b79c
Taylor <mckinneytl@appstate.edu>
2019-10-22 16:08:59 -0400

    impl DecodeStage

---
+++ DecodeStage.C
@@ -0,0 +1,31 @@
+#include <string>
+#include <cstdint>
+#include "RegisterFile.h"
+#include "PipeRegField.h"
+#include "PipeReg.h"
+#include "F.h"
+#include "D.h"
+#include "E.h"
+#include "M.h"
+#include "W.h"
+#include "Stage.h"
+#include "DecodeStage.h"
+#include "Status.h"
+#include "Debug.h"
+
+
+
+
+
+
+
+
+
+
+
+
+void setEInput(E * ereg, uint64_t stat, uint64_t icode, uint64_t ifun,
+        uint64_t valC, uint64_t valA, uint64_t valB, uint64_t dstE,
+        uint64_t dstM, uint64_t srcA, uint64_t srcB)
+{
+}
---
+++ DecodeStage.h
@@ -0,0 +1,13 @@
+//class to perform the combinational logic of
+////the Decode stage
+class DecodeStage: public Stage
+{
+    private:
+        void setEInput(E * ereg, uint64_t stat, uint64_t icode, uint64_t ifun,
+                uint64_t valC, uint64_t valA, uint64_t valB, uint64_t dstE, 
+                uint64_t dstM, uint64_t srcA, uint64_t srcB); 
+    public:
+        bool doClockLow(PipeReg ** pregs, Stage ** stages);
+        void doClockHigh(PipeReg ** pregs);
+
+};
