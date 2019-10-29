8b2399dca5a5a6c57be30a151636c67e763c0f72
taylormckinney <31255100+taylormckinney@users.noreply.github.com>
2019-10-29 17:45:42 -0400

    Update ExecuteStage.C

--- ExecuteStage.C
+++ ExecuteStage.C
@@ -1,1 +1,74 @@
-//execute.C
+#include <string>
+#include <cstdint>
+#include "RegisterFile.h"
+#include "PipeRegField.h"
+#include "PipeReg.h"
+#include "F.h"
+#include "D.h"
+#include "E.h"
+#include "M.h"
+#include "W.h"
+#include "Stage.h"
+#include "ExecuteStage.h"
+#include "Status.h"
+#include "Debug.h"
+
+/*
+ * doClockLow:
+ * Performs the Execute stage combinational logic that is performed when
+ * the clock edge is low.
+ *
+ * @param: pregs - array of the pipeline register sets (F, D, E, M, W instances)
+ * @param: stages - array of stages (FetchStage, DecodeStage, ExecuteStage,
+ *         MemoryStage, WritebackStage instances)
+ */
+bool ExecuteStage::doClockLow(PipeReg ** pregs, Stage ** stages)
+{
+      E * ereg = (E*) pregs[EREG];
+      M * mreg = (M*) pregs[MREG];
+  
+      uint64_t stat = ereg->getstat()->getOutput();
+      uint64_t icode = ereg->geticode()->getOutput();
+      uint64_t Cnd = ereg->getCnd()->getOutput();
+      uint64_t valE = 0;
+      uint64_t valA = ereg->getvalA()->getOutput();
+      uint64_t dstE = ereg->getdstE()->getOutput();
+      uint64_t dstM = ereg->getdstM()->getOutput();
+        
+        setMInput(mreg, stat, icode, Cnd, valE, valA, dstE, dstM);
+        return false;
+}
+/* doClockHigh
+ * applies the appropriate control signal to the register intances
+ *
+ * @param: pregs - array of the pipeline register (F, D, E, M, W instances)
+ */
+void DecodeStage::doClockHigh(PipeReg ** pregs)
+{
+        M * mreg = (M*) pregs[MREG];
+        mreg->getstat()->normal();
+        mreg->geticode()->normal();
+        mreg->getCnd()->normal();
+        mreg->getvalE()->normal();
+        mreg->getvalA()->normal();
+        mreg->getdstE()->normal();
+        mreg->getdstM()->normal();
+}
+/* setMInput
+ * provides the input to potentially be stored in the M register
+ * during doClockHigh
+ *
+
+*/
+void DecodeStage::setMInput(M * mreg, uint64_t stat, uint64_t icode, uint64_t Cnd, uint64_t valE, 
+      uint64_t valA, uint64_t dstE, uint64_t dstM)
+{
+        mreg->getstat()->setInput(stat);
+        mreg->geticode()->setInput(icode);
+        mreg->getCnd()->setInput(Cnd);
+        mreg->getvalE()->setInput(valE);
+        mreg->getvalA()->setInput(valA);
+        mreg->getdstE()->setInput(dstE);
+        mreg->getdstM()->setInput(dstM);
+            
+}
