fdaf12701147c07cd9de155f1d81ad7463f08bc4
taylormckinney <31255100+taylormckinney@users.noreply.github.com>
2019-10-29 17:07:50 -0400

    create Loader.h

---
+++ Loader.h
@@ -0,0 +1,16 @@
+
+class Loader
+{
+   private:
+      bool loaded;        //set to true if a file is successfully loaded into memory
+      std::ifstream inf;  //input file handle 
+   public:
+      Loader(int argc, char * argv[]);
+      bool isLoaded();
+      bool isValidFileName(std::string fname);
+      void loadLine(std::string line);
+      uint64_t convertHex(std::string line, int begin, int end);
+      bool hasErrors(std::string line);    
+      bool validHex(std::string line, int start, int end);
+      bool isCommentLine(std::string line);
+};
