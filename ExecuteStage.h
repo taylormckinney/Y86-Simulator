6b6ffea4726441fb9929c37f302b338da5f24ece
taylormckinney <31255100+taylormckinney@users.noreply.github.com>
2019-10-29 17:25:46 -0400

    Update ExecuteStage.h

--- ExecuteStage.h
+++ ExecuteStage.h
@@ -2,4 +2,10 @@
 
 class ExecuteStage: public Stage
 {
-}
+   private:
+      void setMInput(D * dreg, uint64_t stat, uint64_t icode, uint64_t Cnd, uint64_t valE, 
+      uint64_t valA, uint64_t dstE, uint64_t dstM);
+   public:
+      bool doClockLow(PipeReg ** pregs, Stage ** stages);
+      void doClockHigh(PipeReg ** pregs);
+};
