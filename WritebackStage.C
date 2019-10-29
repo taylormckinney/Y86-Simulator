2050cabc65ee40bce5304247a3914525f3c9ba65
taylormckinney <31255100+taylormckinney@users.noreply.github.com>
2019-10-29 17:53:05 -0400

    Update WritebackStage.C

--- WritebackStage.C
+++ WritebackStage.C
@@ -1,1 +1,39 @@
 //wb.C
+#include <string>
+#include <cstdint>
+#include "RegisterFile.h"
+#include "PipeRegField.h"
+#include "PipeReg.h"
+#include "F.h"
+#include "D.h"
+#include "E.h"
+#include "M.h"
+#include "W.h"
+#include "Stage.h"
+#include "WritebackStage.h"
+#include "Status.h"
+#include "Debug.h"
+
+/*
+ * doClockLow:
+ * Performs the Execute stage combinational logic that is performed when
+ * the clock edge is low.
+ *
+ * @param: pregs - array of the pipeline register sets (F, D, E, M, W instances)
+ * @param: stages - array of stages (FetchStage, DecodeStage, ExecuteStage,
+ *         MemoryStage, WritebackStage instances)
+ */
+bool ExecuteStage::doClockLow(PipeReg ** pregs, Stage ** stages)
+{
+    
+        return true;
+}
+/* doClockHigh
+ * applies the appropriate control signal to the register intances
+ *
+ * @param: pregs - array of the pipeline register (F, D, E, M, W instances)
+ */
+void DecodeStage::doClockHigh(PipeReg ** pregs)
+{
+       
+}
