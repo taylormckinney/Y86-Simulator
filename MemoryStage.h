4ad983c52bc6462829af1a71551e8157467f35e1
taylormckinney <31255100+taylormckinney@users.noreply.github.com>
2019-10-29 17:27:36 -0400

    Update MemoryStage.h

--- MemoryStage.h
+++ MemoryStage.h
@@ -1,4 +1,11 @@
 //mem.h
 class MemoryStage: public Stage
 {
-}
+   private:
+      void setWInput(D * dreg, uint64_t stat, uint64_t icode, uint64_t valE, uint64_t valM, 
+      uint64_t dstE, uint64_t dstM);
+      
+   public:
+      bool doClockLow(PipeReg ** pregs, Stage ** stages);
+      void doClockHigh(PipeReg ** pregs);
+};
