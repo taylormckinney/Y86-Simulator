163570c5a40394d08947fb960c86caa35bae92bc
ernhartcl <54417176+ernhartcl@users.noreply.github.com>
2019-12-06 16:39:57 -0500

    reformatted for consistency and readability

--- F.h
+++ F.h
@@ -1,10 +1,11 @@
 //class to hold the F pipeline registers
-class F : public PipeReg
-{
-   private:
-      PipeRegField * predPC;
-   public:
-      F();
-      PipeRegField * getpredPC();
-      void dump();
+class F : public PipeReg {
+private:
+    PipeRegField *predPC;
+public:
+    F();
+
+    PipeRegField *getpredPC();
+
+    void dump();
 };
