41d0fb986b655a3c9c5a047d36a308875500649b
Cori Ernhart <ernhartcl@appstate.edu>
2019-12-06 14:51:38 -0500

    updated to reflect new MemoryStage methods

--- MemoryStage.h
+++ MemoryStage.h
@@ -8,4 +8,7 @@ class MemoryStage: public Stage
    public:
       bool doClockLow(PipeReg ** pregs, Stage ** stages);
       void doClockHigh(PipeReg ** pregs);
+      uint64_t memAddr(uint64_t instr, uint64_t M_valE, uint64_t M_valA);
+      bool memRead(uint64_t instr);
+      bool memWrite(uint64_t instr);
 };
