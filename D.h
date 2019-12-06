9f9a38df61168ac5d653ecc7db557110bbf0d9b6
ernhartcl <54417176+ernhartcl@users.noreply.github.com>
2019-12-06 16:33:56 -0500

    reformatted for consistency and readability

--- D.h
+++ D.h
@@ -1,22 +1,29 @@
 //class to hold the D pipeline registers
-class D : public PipeReg
-{
-   private:
-      PipeRegField * stat;
-      PipeRegField * icode;
-      PipeRegField * ifun;
-      PipeRegField * rA;
-      PipeRegField * rB;
-      PipeRegField * valC;
-      PipeRegField * valP;
-   public:
-      D();
-      PipeRegField * getstat();
-      PipeRegField * geticode();
-      PipeRegField * getifun();
-      PipeRegField * getrA();
-      PipeRegField * getrB();
-      PipeRegField * getvalC();
-      PipeRegField * getvalP();
-      void dump();
+class D : public PipeReg {
+private:
+    PipeRegField *stat;
+    PipeRegField *icode;
+    PipeRegField *ifun;
+    PipeRegField *rA;
+    PipeRegField *rB;
+    PipeRegField *valC;
+    PipeRegField *valP;
+public:
+    D();
+
+    PipeRegField *getstat();
+
+    PipeRegField *geticode();
+
+    PipeRegField *getifun();
+
+    PipeRegField *getrA();
+
+    PipeRegField *getrB();
+
+    PipeRegField *getvalC();
+
+    PipeRegField *getvalP();
+
+    void dump();
 };
