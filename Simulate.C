fe185d2f222f940e450ffd759b075c0414f03c70
ernhartcl <54417176+ernhartcl@users.noreply.github.com>
2019-12-06 16:56:37 -0500

    reformatted for consistency and readability

--- Simulate.C
+++ Simulate.C
@@ -8,7 +8,7 @@
  * to represent the pipelined registers that provide the input and
  * accept the output of these stages.
 */
- 
+
 #include <iomanip>
 #include <iostream>
 #include "PipeRegField.h"
@@ -28,28 +28,28 @@
 #include "FetchStage.h"
 #include "WritebackStage.h"
 #include "Simulate.h"
+
 /*
  * Simulate constructor
  *
  * creates instances of each data member
 */
-Simulate::Simulate()
-{
-   /* PIPE stages */
-   stages = new Stage * [NUMSTAGES];
-   stages[FSTAGE] = new FetchStage();
-   stages[DSTAGE] = new DecodeStage();
-   stages[ESTAGE] = new ExecuteStage();
-   stages[MSTAGE] = new MemoryStage();
-   stages[WSTAGE] = new WritebackStage();
+Simulate::Simulate() {
+    /* PIPE stages */
+    stages = new Stage *[NUMSTAGES];
+    stages[FSTAGE] = new FetchStage();
+    stages[DSTAGE] = new DecodeStage();
+    stages[ESTAGE] = new ExecuteStage();
+    stages[MSTAGE] = new MemoryStage();
+    stages[WSTAGE] = new WritebackStage();
 
-   /* pipelined registers */
-   pregs = new PipeReg * [NUMPIPEREGS];
-   pregs[FREG] = new F();
-   pregs[DREG] = new D();
-   pregs[EREG] = new E();
-   pregs[MREG] = new M();
-   pregs[WREG] = new W();
+    /* pipelined registers */
+    pregs = new PipeReg *[NUMPIPEREGS];
+    pregs[FREG] = new F();
+    pregs[DREG] = new D();
+    pregs[EREG] = new E();
+    pregs[MREG] = new M();
+    pregs[WREG] = new W();
 }
 
 /* 
@@ -57,29 +57,27 @@ Simulate::Simulate()
  * 
  * Simulate the stages of the PIPE machine until a halt is executed.
 */
-void Simulate::run()
-{
-   int cycle = 0;
-   Memory * mem = Memory::getInstance();
-   ConditionCodes * cc = ConditionCodes::getInstance();
-   RegisterFile * rf = RegisterFile::getInstance();
-   bool stop = false;
+void Simulate::run() {
+    int cycle = 0;
+    Memory *mem = Memory::getInstance();
+    ConditionCodes *cc = ConditionCodes::getInstance();
+    RegisterFile *rf = RegisterFile::getInstance();
+    bool stop = false;
 
-   while (!stop)
-   {
-      stop = doClockLow();
-      doClockHigh();
+    while (!stop) {
+        stop = doClockLow();
+        doClockHigh();
 
-      /* dump the values of the pipelined registers, Condition Codes, */
-      /* Register File, and Memory */
-      std::cout << "\nAt end of cycle " << std::dec 
-         << cycle << ":" << std::endl;
-      dumpPipeRegs();
-      cc->dump();
-      rf->dump();
-      mem->dump();
-      cycle++;
-   }
+        /* dump the values of the pipelined registers, Condition Codes, */
+        /* Register File, and Memory */
+        std::cout << "\nAt end of cycle " << std::dec
+                  << cycle << ":" << std::endl;
+        dumpPipeRegs();
+        cc->dump();
+        rf->dump();
+        mem->dump();
+        cycle++;
+    }
 }
 
 /*
@@ -88,18 +86,17 @@ void Simulate::run()
  * When the clock is low, the stages use their inputs
  * to calculate the outputs.
 */
-bool Simulate::doClockLow()
-{
-   bool stop;
+bool Simulate::doClockLow() {
+    bool stop;
 
-   //going through the stages in reverse order helps to
-   //simulate the parallel behavior of the hardware
-   stop = stages[WSTAGE]->doClockLow(pregs, stages);
-   stages[MSTAGE]->doClockLow(pregs, stages);
-   stages[ESTAGE]->doClockLow(pregs, stages);
-   stages[DSTAGE]->doClockLow(pregs, stages);
-   stages[FSTAGE]->doClockLow(pregs, stages);
-   return stop;
+    //going through the stages in reverse order helps to
+    //simulate the parallel behavior of the hardware
+    stop = stages[WSTAGE]->doClockLow(pregs, stages);
+    stages[MSTAGE]->doClockLow(pregs, stages);
+    stages[ESTAGE]->doClockLow(pregs, stages);
+    stages[DSTAGE]->doClockLow(pregs, stages);
+    stages[FSTAGE]->doClockLow(pregs, stages);
+    return stop;
 }
 
 /*
@@ -110,22 +107,21 @@ bool Simulate::doClockLow()
  * The code to update the pipelined registers is in the
  * pipe stage class.
 */
-void Simulate::doClockHigh()
-{
-   //get the WritebackStage to update the register file
-   stages[WSTAGE]->doClockHigh(pregs);
+void Simulate::doClockHigh() {
+    //get the WritebackStage to update the register file
+    stages[WSTAGE]->doClockHigh(pregs);
 
-   //get the MemoryStage to update the W register
-   stages[MSTAGE]->doClockHigh(pregs);
+    //get the MemoryStage to update the W register
+    stages[MSTAGE]->doClockHigh(pregs);
 
-   //get the ExecuteStage to update the M register
-   stages[ESTAGE]->doClockHigh(pregs);
+    //get the ExecuteStage to update the M register
+    stages[ESTAGE]->doClockHigh(pregs);
 
-   //get the DecodeStage to update the E register
-   stages[DSTAGE]->doClockHigh(pregs);
+    //get the DecodeStage to update the E register
+    stages[DSTAGE]->doClockHigh(pregs);
 
-   //get the FetchStage to update the F and D registers
-   stages[FSTAGE]->doClockHigh(pregs);
+    //get the FetchStage to update the F and D registers
+    stages[FSTAGE]->doClockHigh(pregs);
 }
 
 /*
@@ -133,11 +129,10 @@ void Simulate::doClockHigh()
  *
  * Display the contents of the pipelined registers
 */
-void Simulate::dumpPipeRegs()
-{
-   pregs[FREG]->dump();
-   pregs[DREG]->dump();
-   pregs[EREG]->dump();
-   pregs[MREG]->dump();
-   pregs[WREG]->dump();
+void Simulate::dumpPipeRegs() {
+    pregs[FREG]->dump();
+    pregs[DREG]->dump();
+    pregs[EREG]->dump();
+    pregs[MREG]->dump();
+    pregs[WREG]->dump();
 }
