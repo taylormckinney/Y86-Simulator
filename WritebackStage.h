e845de43298ee866c44fe35f75c74ef1b4be94d9
ernhartcl <54417176+ernhartcl@users.noreply.github.com>
2019-12-06 17:05:45 -0500

    reformatted for consistency and readability

--- WritebackStage.h
+++ WritebackStage.h
@@ -1,9 +1,9 @@
 //wb.h
-class WritebackStage: public Stage
-{
- private:
-      RegisterFile * reg = RegisterFile::getInstance();
- public:
-      bool doClockLow(PipeReg ** pregs, Stage ** stages);
-      void doClockHigh(PipeReg ** pregs);
+class WritebackStage : public Stage {
+private:
+    RegisterFile *reg = RegisterFile::getInstance();
+public:
+    bool doClockLow(PipeReg **pregs, Stage **stages);
+
+    void doClockHigh(PipeReg **pregs);
 };
