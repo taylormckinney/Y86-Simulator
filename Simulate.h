9b90c896141ade2313c091cbb77f25b84974702b
ernhartcl <54417176+ernhartcl@users.noreply.github.com>
2019-12-06 16:57:29 -0500

    reformatted for consistency and readability

--- Simulate.h
+++ Simulate.h
@@ -1,13 +1,16 @@
 //Driver class for the yess simulator
-class Simulate
-{
-   private:
-      PipeReg ** pregs;
-      Stage ** stages;
-   public:
-      Simulate();
-      void run();
-      bool doClockLow();
-      void doClockHigh();
-      void dumpPipeRegs();
+class Simulate {
+private:
+    PipeReg **pregs;
+    Stage **stages;
+public:
+    Simulate();
+
+    void run();
+
+    bool doClockLow();
+
+    void doClockHigh();
+
+    void dumpPipeRegs();
 };
