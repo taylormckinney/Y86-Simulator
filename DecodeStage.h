64583cfff1b9d6e310ba7c98ea47201f56cd952f
ernhartcl <54417176+ernhartcl@users.noreply.github.com>
2019-12-08 20:12:24 -0500

    reformatted for consistency and readability

--- DecodeStage.h
+++ DecodeStage.h
@@ -1,29 +1,39 @@
 //class to perform the combinational logic of
 ////the Decode stage
-class DecodeStage: public Stage
-{
-    private:
-        void setEInput(E * ereg, uint64_t stat, uint64_t icode, uint64_t ifun,
-                uint64_t valC, uint64_t valA, uint64_t valB, uint64_t dstE, 
-                uint64_t dstM, uint64_t srcA, uint64_t srcB); 
+class DecodeStage : public Stage {
+private:
+    void setEInput(E *ereg, uint64_t stat, uint64_t icode, uint64_t ifun,
+                   uint64_t valC, uint64_t valA, uint64_t valB, uint64_t dstE,
+                   uint64_t dstM, uint64_t srcA, uint64_t srcB);
 
-        RegisterFile * reg = RegisterFile::getInstance();
-        bool regError;
-        uint64_t d_srcA, d_srcB;
-        bool E_bubble;
+    RegisterFile *reg = RegisterFile::getInstance();
+    bool regError;
+    uint64_t d_srcA, d_srcB;
+    bool E_bubble;
 
-    public:
-        bool doClockLow(PipeReg ** pregs, Stage ** stages);
-        void doClockHigh(PipeReg ** pregs);
-        uint64_t getSrcA(uint64_t instr, uint64_t D_rA);
-        uint64_t getSrcB(uint64_t instr, uint64_t D_rB);
-        uint64_t getDstM(uint64_t instr, uint64_t D_rA);
-        uint64_t getDstE(uint64_t instr, uint64_t D_rB);
-        uint64_t selFwdA(PipeReg ** pregs, Stage ** stages);
-        uint64_t forwardB(PipeReg ** pregs, Stage ** stages);
-        uint64_t getd_srcA(){return d_srcA;};
-        uint64_t getd_srcB(){return d_srcB;};
-        void calculateControlSignals(PipeReg ** pregs, Stage ** stages);
-        // for jxx implementation
-        bool gete_bubble(uint64_t e_icode, uint64_t e_Cnd, uint64_t e_dstM);
+public:
+    bool doClockLow(PipeReg **pregs, Stage **stages);
+
+    void doClockHigh(PipeReg **pregs);
+
+    uint64_t getSrcA(uint64_t instr, uint64_t D_rA);
+
+    uint64_t getSrcB(uint64_t instr, uint64_t D_rB);
+
+    uint64_t getDstM(uint64_t instr, uint64_t D_rA);
+
+    uint64_t getDstE(uint64_t instr, uint64_t D_rB);
+
+    uint64_t selFwdA(PipeReg **pregs, Stage **stages);
+
+    uint64_t forwardB(PipeReg **pregs, Stage **stages);
+
+    uint64_t getd_srcA() { return d_srcA; };
+
+    uint64_t getd_srcB() { return d_srcB; };
+
+    void calculateControlSignals(PipeReg **pregs, Stage **stages);
+
+    // for jxx implementation
+    bool gete_bubble(uint64_t e_icode, uint64_t e_Cnd, uint64_t e_dstM);
 };
